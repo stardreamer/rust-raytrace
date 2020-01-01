a9f42d99fa44d9d8b80f9b217bbfc155409de7b4
Arthur Asylgareev <arthur@asylgareev>
2020-01-01 21:35:16 +0500

    Basic helloworld example  was remade using Vec3 instead of floats

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+mod structs;
+
 use std::fs::File;
 use std::io::{Error, Write};
+use structs::vec3::Vec3;
 
 fn main() {
     let mut file = File::create("img.ppm").expect("Unable to create file!");
@@ -8,16 +11,11 @@ fn main() {
     writeln!(&mut file, "P3\n {} {} \n255", nx, ny).expect("Unable to write to file!");
     for j in (0..ny).rev() {
         for i in 0..nx {
-            println!("{} {}", i, j);
-            let r = (i as f64) / (nx as f64);
-            let g = (j as f64) / (ny as f64);
-            let b = 0.2_f64;
-
-            let ir = (255.99 * r) as i64;
-            let ig = (255.99 * g) as i64;
-            let ib = (255.99 * b) as i64;
+            let col = Vec3::new((i as f64) / (nx as f64), (j as f64) / (ny as f64), 0.2_f64);
+            let ir = (255.99 * col.r()) as i64;
+            let ig = (255.99 * col.g()) as i64;
+            let ib = (255.99 * col.b()) as i64;
             writeln!(&mut file, "{} {} {}", ir, ig, ib).expect("Unable to write to file");
         }
     }
-    println!("Hello, world!");
 }
