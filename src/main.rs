c42a71f81274dc134a7771a147b1b95d87b30730
Arthur Asylgareev <arthur@asylgareev>
2020-01-03 17:06:31 +0500

    Main function now works with hittablelist instead of a single sphere

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,25 @@
+mod obj;
 mod structs;
 
+use obj::hittablelist::HittableList;
+use obj::sphere::Sphere;
+use obj::Hittable;
 use std::fs::File;
-use std::io::{Error, Write};
-use std::option;
+use std::io::Write;
 use structs::ray::Ray;
 use structs::vec3::Vec3;
 
-fn hit_sphere(center: Vec3, radius: f64, ray: &Ray) -> Option<f64> {
-    let oc = ray.origin - center;
-    let a = ray.direction.dot(&ray.direction);
-    let b = 2_f64 * oc.dot(&ray.direction);
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = (b * b - 4_f64 * a * c);
+fn color(ray: &Ray, world: &HittableList) -> Vec3 {
+    match world.hit(ray, 0_f64, std::f64::MAX) {
+        Some(hit) => 0.5_f64 * Vec3::new(hit.n.x() + 1_f64, hit.n.y() + 1_f64, hit.n.z() + 1_f64),
+        None => {
+            let unit_direction = ray.direction.unit_vector();
 
-    if (discriminant < 0_f64) {
-        None
-    } else {
-        Some((-b - discriminant.sqrt()) / (2_f64 * a))
-    }
-}
+            let t = 0.5_f64 * (unit_direction.y() + 1_f64);
 
-fn color(ray: &Ray) -> Vec3 {
-    match hit_sphere(Vec3::new(0_f64, 0_f64, -1_f64), 0.5_f64, ray) {
-        Some(t) => {
-            let n = (ray.point_at(t) - Vec3::new(0_f64, 0_f64, -1_f64)).unit_vector();
-            return 0.5_f64 * Vec3::new(n.x() + 1_f64, n.y() + 1_f64, n.z() + 1_f64);
+            (1_f64 - t) * Vec3::new(1_f64, 1_f64, 1_f64) + t * Vec3::new(0.5_f64, 0.7_f64, 1.0_f64)
         }
-        None => (),
     }
-
-    let unit_direction = ray.direction.unit_vector();
-
-    let t = 0.5_f64 * (unit_direction.y() + 1_f64);
-
-    (1_f64 - t) * Vec3::new(1_f64, 1_f64, 1_f64) + t * Vec3::new(0.5_f64, 0.7_f64, 1.0_f64)
 }
 
 fn main() {
@@ -53,7 +39,15 @@ fn main() {
             let v = (j as f64) / (ny as f64);
 
             let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
-            let col = color(&r);
+
+            let world = HittableList {
+                objects: vec![
+                    Box::new(Sphere::new(Vec3::new(0_f64, 0_f64, -1_f64), 0.5)),
+                    Box::new(Sphere::new(Vec3::new(0_f64, -100.5_f64, -1_f64), 100_f64)),
+                ],
+            };
+
+            let col = color(&r, &world);
             let (ir, ig, ib) = col.irgb(255.99_f64);
             writeln!(&mut file, "{} {} {}", ir, ig, ib).expect("Unable to write to file");
         }
