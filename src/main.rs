1be5dfff58f2fcfebf3be0176378fb09eef3f45c
Arthur Asylgareev <arthur@asylgareev>
2020-01-06 15:29:09 +0500

    Viewpoint can now be set for camera

--- src/main.rs
+++ src/main.rs
@@ -39,20 +39,43 @@ fn color(ray: &Ray, world: &HittableList, rng: &mut impl Iterator<Item = f64>, d
     }
 }
 
+fn get_cameras(
+    look_at: Vec3,
+    r: f64,
+    v_fov: f64,
+    aspect: f64,
+    speed: i32,
+) -> impl Iterator<Item = Camera> {
+    (0..180 * speed)
+        .map(move |a| ((a as f64) / (speed as f64)) * std::f64::consts::PI / 180_f64)
+        .map(move |ar| {
+            let v_up = if ar <= std::f64::consts::PI / 2_f64 {
+                Vec3::new(0_f64, 1_f64, 0_f64)
+            } else {
+                Vec3::new(0_f64, -1_f64, 0_f64)
+            };
+            Camera::new(
+                Vec3::new(
+                    0_f64,
+                    look_at.y() + r * ar.sin(),
+                    look_at.z() + r * ar.cos(),
+                ),
+                look_at,
+                v_up,
+                v_fov,
+                aspect,
+            )
+        })
+}
+
 fn main() {
     let mut g1 = random::frand(32);
     let mut g2 = random::frand(44);
     let mut g = random::frand(53);
 
-    let mut file = File::create("img-anti.ppm").expect("Unable to create file!");
     let nx = 400;
     let ny = 200;
     let ns = 100;
-
-    writeln!(&mut file, "P3\n {} {} \n255", nx, ny).expect("Unable to write to file!");
-
-    let camera = Camera::default();
-
     let world = HittableList {
         objects: vec![
             Box::new(Sphere::new(
@@ -94,31 +117,45 @@ fn main() {
         ],
     };
 
-    for j in (0..ny).rev() {
-        for i in 0..nx {
-            let r_rays = g1
-                .by_ref()
-                .take(ns)
-                .zip(g2.by_ref().take(ns))
-                .map(|(r1, r2)| {
-                    (
-                        ((i as f64) + r1) / (nx as f64),
-                        ((j as f64) + r2) / (ny as f64),
-                    )
-                })
-                .map(|(u, v)| camera.get_ray(u, v));
+    for (frame_number, camera) in get_cameras(
+        Vec3::new(0_f64, 0_f64, -1_f64),
+        1_f64,
+        90_f64,
+        (nx as f64) / (ny as f64),
+        1,
+    )
+    .enumerate()
+    {
+        let mut file = File::create(format!("video/{:0>8}.ppm", frame_number))
+            .expect("Unable to create file!");
+        writeln!(&mut file, "P3\n {} {} \n255", nx, ny).expect("Unable to write to file!");
 
-            let mut col = Vec3::default();
-            for ray in r_rays {
-                col += color(&ray, &world, &mut g, 0);
+        for j in (0..ny).rev() {
+            for i in 0..nx {
+                let r_rays = g1
+                    .by_ref()
+                    .take(ns)
+                    .zip(g2.by_ref().take(ns))
+                    .map(|(r1, r2)| {
+                        (
+                            ((i as f64) + r1) / (nx as f64),
+                            ((j as f64) + r2) / (ny as f64),
+                        )
+                    })
+                    .map(|(u, v)| camera.get_ray(u, v));
+
+                let mut col = Vec3::default();
+                for ray in r_rays {
+                    col += color(&ray, &world, &mut g, 0);
+                }
+
+                col /= ns as f64;
+
+                col = Vec3::new(col.r().sqrt(), col.g().sqrt(), col.b().sqrt());
+
+                let (ir, ig, ib) = col.irgb(255.99_f64);
+                writeln!(&mut file, "{} {} {}", ir, ig, ib).expect("Unable to write to file");
             }
-
-            col /= ns as f64;
-
-            col = Vec3::new(col.r().sqrt(), col.g().sqrt(), col.b().sqrt());
-
-            let (ir, ig, ib) = col.irgb(255.99_f64);
-            writeln!(&mut file, "{} {} {}", ir, ig, ib).expect("Unable to write to file");
         }
     }
 }
--- src/obj/camera.rs
+++ src/obj/camera.rs
@@ -13,8 +13,25 @@ impl Camera {
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         Ray::new(
             self.origin,
-            self.lower_left_corner + u * self.horizontal + v * self.vertical,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
         )
+    }
+
+    pub fn new(look_from: Vec3, look_at: Vec3, v_up: Vec3, v_fov: f64, aspect: f64) -> Self {
+        let theta = v_fov * std::f64::consts::PI / 180_f64;
+        let half_height = (theta / 2_f64).tan();
+        let half_width = aspect * half_height;
+
+        let w = (look_from - look_at).unit_vector();
+        let u = v_up.cross(&w).unit_vector();
+        let v = w.cross(&u);
+
+        Self {
+            lower_left_corner: look_from - half_width * u - half_height * v - w,
+            horizontal: 2_f64 * half_width * u,
+            vertical: 2_f64 * half_height * v,
+            origin: look_from,
+        }
     }
 }
 
